60dd03b5a153b870dfcf6c370e233e7916a2b8d7
ergincetn01 <ergincetn01@gmail.com>
2023-06-26 14:18:24 +0300

    all comps impl.

---
+++ src/Components/BookEdit.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function BookEdit({book, onSubmit}){
+   const[title,setTitle]=useState(book.title);
+
+   const handleChange=(e)=> {
+    setTitle(e.target.value)
+   }
+
+   const handleSubmit=(e)=> {
+        e.preventDefault();
+        onSubmit(book.id, title);
+   }
+    return (
+        <div>
+            <form className="book-edit" onSubmit={handleSubmit}>
+            <label> 
+                Title
+            </label>
+            <input onChange={handleChange} value={title} className="input"  />
+            <button className="button is-primary">Save</button>
+            </form>
+        </div>
+    )
+
+}
+
+export default BookEdit;
